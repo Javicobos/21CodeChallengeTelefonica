1c3bf63cf8d6903fe2afdb6c88deb44063248759
Javicobos <sirjavicobos@gmail.com>
2021-12-17 19:00:36 +0100

    'solved' 8 but complexity is really bad
    this works for the test cases (takes <5 seconds) but not for the submit phase so I have to make something smarter

--- 8CityBuses/8CityBuses.py
+++ 8CityBuses/8CityBuses.py
@@ -1,21 +1,10 @@
 lines = []
-with open('testInput.txt', 'r') as f:
+with open('submitInput.txt', 'r') as f:
 	lines = f.readlines()
 
-outputFile = open('testOutput.txt', 'w')
+outputFile = open('submitOutput.txt', 'w')
 
 totalCases = int(lines[0])
-
-for i in range(totalCases): #do I even want this loop? dunno!
-
-
-
-
-
-	outputFile.write('Case #' + str(i + 1) + ': ' + 'WIP')
-	if (i != totalCases - 1):
-		outputFile.write('\n')
-
 
 #some initial thoughts: (these may not be updated after I code things)
 #this could work like
@@ -34,3 +23,133 @@ for i in range(totalCases): #do I even want this loop? dunno!
 # 		Listing all trips sounds difficult because you'd get a bazillon options if the graph has many connections maybe
 # 		Like you can't just get the shortest one or the longest one, it seems intuitively less easy	
 
+#	2nd day idea
+# 	going to set up arrays for each city like [Madrid,1,1,0,0] that tell me what they are directly linked to
+# 		aferwards we check each of a city's connections and add its connections as well
+# 		we repeat this until there are no new connections	
+	
+def makeInitialMatrix(firstLineIndex, totalTrips):
+	initialCityMatrix = []
+	#simpleCityList = [] #some weird stuff happening here 
+	for busTrip in range(totalTrips):												#we set the thing up with the cities' names
+		firstCityCandidate = lines[firstLineIndex + busTrip].partition(',')[0]
+		secondCityCandidate = lines[firstLineIndex + busTrip].partition(',')[2].replace('\n','')
+		if firstCityCandidate not in (x[0] for x in initialCityMatrix):
+			initialCityMatrix.append([firstCityCandidate])
+			simpleCityList.append(firstCityCandidate)
+		if secondCityCandidate not in (x[0] for x in initialCityMatrix):
+			initialCityMatrix.append([secondCityCandidate])
+			simpleCityList.append(secondCityCandidate)
+
+	myTotalCities = len(initialCityMatrix)
+	for city in initialCityMatrix:													#add some zeroes that will be filled up
+		for unit in range(myTotalCities):
+			city.append(0)
+	
+	for busTrip in range(totalTrips):
+		firstCityCandidate = lines[firstLineIndex + busTrip].partition(',')[0]
+		secondCityCandidate = lines[firstLineIndex + busTrip].partition(',')[2].replace('\n','')
+		firstCityIndex = simpleCityList.index(firstCityCandidate)
+		secondCityIndex = simpleCityList.index(secondCityCandidate)
+		initialCityMatrix[firstCityIndex][firstCityIndex + 1] = 2
+		initialCityMatrix[secondCityIndex][secondCityIndex + 1] = 2 #give each a 2 for self
+		initialCityMatrix[firstCityIndex][secondCityIndex +1] = 1
+		initialCityMatrix[secondCityIndex][firstCityIndex +1] = 1 #give each the 1 for the other city
+
+
+	#print simpleCityList
+	return initialCityMatrix
+
+#print simpleCityList
+
+import copy
+
+#myTest = makeInitialMatrix(9, 16)
+#myTest2 = copy.deepcopy(myTest)
+#myTest2[0][0] = 'Wakanda'
+#print myTest
+#print myTest2
+#print simpleCityList
+
+def removeCity(cityToRemove, copyCityMatrix):
+	cityRemoveIndex = simpleCityList.index(cityToRemove)
+	#print cityRemoveIndex
+	for index, elem in enumerate(copyCityMatrix[cityRemoveIndex][1:]):
+		copyCityMatrix[cityRemoveIndex][index + 1] = 5					#5 is our special value for deleted cities, because why not
+	#print copyCityMatrix[cityRemoveIndex]
+	#return copyCityMatrix
+
+#removeCity('Atoka', myTest)
+#print 'city removed matrix'
+#print myTest
+
+def makeDeepConnections(removedCity, passedCityMatrix):
+	removedCityIndex = simpleCityList.index(removedCity)
+	copyCityMatrix = copy.deepcopy(passedCityMatrix)
+	while True:
+		lastCityMatrix = copy.deepcopy(copyCityMatrix)
+		for index, elem in enumerate(copyCityMatrix):				#index is city index
+			if index != removedCityIndex:
+				for index2, connection in enumerate(copyCityMatrix[index][1:]):		#index2 will iterate thru connections
+					if connection == 1:
+						for index3, endConnection in enumerate(copyCityMatrix[index2][1:]):
+							if endConnection == 1:
+								if copyCityMatrix[index][index3 + 1] == 0:				#if A is connected to B, we give B's connections to A too
+									copyCityMatrix[index][index3 + 1] = 1
+		if lastCityMatrix == copyCityMatrix:
+			break
+		#print 'go agane'
+		#print copyCityMatrix
+	return copyCityMatrix
+
+#makeDeepConnections('Atoka', myTest) 
+#print 'frst connections'
+#print myTest
+
+#makeDeepConnections('Atoka', myTest) 
+#print 'second connections'
+#print myTest
+offset = 1
+for i in range(totalCases): #do I even want this loop? dunno!
+	#print 'uwu ' + lines[i + offset]
+	trips = int(lines[i + offset])
+	#print offset
+	#cityMatrix = makeInitialMatrix
+	#print i + offset + 1
+	#print trips
+	initialCityMatrix = []
+	simpleCityList = []
+	initialCityMatrix = makeInitialMatrix(i + offset + 1, trips)
+	offset += trips
+	
+	criticalCities = []
+	for cityToRemove in simpleCityList:
+		copyCityMatrix = copy.deepcopy(initialCityMatrix)
+		removeCity(cityToRemove, copyCityMatrix)
+		copyCityMatrix = makeDeepConnections(cityToRemove, copyCityMatrix)
+		for cityList in copyCityMatrix:
+			if 0 in cityList:							#detect that the removed city was critical
+				#print cityToRemove
+				#print copyCityMatrix
+				#print cityList
+				criticalCities.append(cityToRemove)
+				break
+
+	outputFile.write('Case #' + str(i + 1) + ': ')
+
+	#print criticalCities
+	if criticalCities:
+		sorted_cities = sorted(criticalCities)
+		needComma = False
+		for cityToWrite in sorted_cities:
+			if needComma:
+				outputFile.write(',')
+			needComma = True
+			outputFile.write(cityToWrite)
+			
+		#print str(sorted_cities)
+	else:
+		outputFile.write('-')
+	
+	if (i != totalCases - 1):
+		outputFile.write('\n')
